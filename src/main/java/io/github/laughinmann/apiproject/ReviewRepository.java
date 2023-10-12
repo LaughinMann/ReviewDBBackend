c763105c87c39fd96db953e4e1490d635ffc02f8
Kelvin Dhoman <kelvin.dhoman@gmail.com>
2023-10-12 17:52:06 -0400

    Initial commit

---
+++ src/main/java/io/github/laughinmann/apiproject/ReviewRepository.java
@@ -0,0 +1,10 @@
+package io.github.laughinmann.apiproject;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
